e22f180ec180c52f53dae97fcf728aeb303b1215
yeve00day-dot <yeve00day@gmail.com>
2026-02-27 14:57:03 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname)));
 
 // 기본 경로 설정
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'tutorial.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
